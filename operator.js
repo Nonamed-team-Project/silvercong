95b30c5d7efb11519f82baea7412e7cf955df059
silvercong <bin091003@naver.com>
2023-09-08 16:08:51 +0900

    operator_pr

---
+++ operator.js
@@ -0,0 +1,17 @@
+console.clear();
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(Math.floor(5 / 2));
+console.log(5 ** 2);
+console.log(Math.pow(5, 2));
+
+console.log("1" == 1);
+console.log("1" === 1);
+console.log("1" != 1);
+console.log("1" !== 1);
+console.clear();
+
+console.log(+false);
+console.log(+null);
+console.log(+"text");
+console.log(+undefined);
